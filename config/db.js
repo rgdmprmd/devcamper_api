8d7065584951515738f7971661996e7f195c90b1
Rangga Dimas Permadi <ranggadpermadi@gmail.com>
2021-11-17 16:03:55 +0700

    Install colors for console coloring

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 	const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true });
-	console.log(`MongoDB Connected: ${conn.connection.host}`);
+	console.log(`MongoDB Connected: ${conn.connection.host}`.cyan);
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 // const logger = require("./middleware/logger");
 
 // DB connection
@@ -24,11 +25,11 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow));
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-	console.log(`Error: ${err.message}`);
+	console.log(`Error: ${err.message}`.red);
 	// close the server & exit process
 	server.close(() => process.exit(1));
 });
