3aa30663b8adb5751de68cf3b19e7159fccebd9e
Rangga Dimas Permadi <ranggadpermadi@gmail.com>
2021-11-18 10:36:57 +0700

    Setup get method for bootcamp controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,29 @@ const Bootcamp = require("../models/Bootcamp");
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public (no need auth)
-exports.getBootcamps = (req, res, next) => {
-	res.status(200).json({ success: true, msg: "Show all bootcamps" });
+exports.getBootcamps = async (req, res, next) => {
+	try {
+		const bootcamps = await Bootcamp.find();
+		res.status(200).json({ success: true, data: bootcamps });
+	} catch (err) {
+		res.status(400).json({ success: false, err: err });
+	}
 };
 
 // @desc    Get single bootcamps
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public (no need auth)
-exports.getBootcamp = (req, res, next) => {
-	res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.findById(req.params.id);
+
+		if (!bootcamp) {
+			return res.status(400).json({ success: false, err: `Bootcamp not exists` });
+		}
+		res.status(200).json({ success: true, data: bootcamp });
+	} catch (err) {
+		res.status(400).json({ success: false, err: err });
+	}
 };
 
 // @desc    Create new bootcamps
