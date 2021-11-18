145c30ca6e7e46a0346b5a44aa92df1404be1b20
Rangga Dimas Permadi <ranggadpermadi@gmail.com>
2021-11-18 11:36:51 +0700

    Handle custom error for duplicate and validation error

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -10,7 +10,7 @@ const BootcampSchema = new mongoose.Schema({
 	slug: String,
 	description: {
 		type: String,
-		required: [true, "Please add a name"],
+		required: [true, "Please add a description"],
 		trim: true,
 		maxlength: [500, "Description can not be more than 500 characters"],
 	},
