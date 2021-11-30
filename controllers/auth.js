b760d040da32ae6b86bb4e2d59bbc20279fbf310
Rangga Dimas Permadi <ranggadpermadi@gmail.com>
2021-11-30 17:22:00 +0700

    Setup send email for forgot password with mailtrap (nice)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
+const sendEmail = require("../utils/sendEmail");
 const User = require("../models/User");
 
 // @desc    Register user
@@ -57,8 +58,33 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
 	// Get reset token
 	const resetToken = user.getResetPasswordToken();
+
 	// Save the user with hashed token and expire token
 	await user.save({ validateBeforeSave: false });
+
+	// Create reset URL
+	const resetUrl = `${req.protocol}://${req.get("host")}/api/v1/resetpassword/${resetToken}`;
+	const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+	try {
+		await sendEmail({
+			email: user.email,
+			subject: `Password reset token`,
+			message,
+		});
+
+		res.status(200).json({ success: true, data: `Email send` });
+	} catch (err) {
+		// if something goes wrong, reset the token and expire in user doc
+		console.error(err);
+
+		user.resetPasswordToken = undefined;
+		user.resetPasswordExpire = undefined;
+
+		await user.save({ validateBeforeSave: false });
+
+		return next(new ErrorResponse(`Email could not be sent`, 500));
+	}
 
 	res.status(200).json({ success: true, data: user });
 });
