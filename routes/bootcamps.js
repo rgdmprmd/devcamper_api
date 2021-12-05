940fc6212aebcc91ce0e02d9ad3fe5f6b3c904bb
rgdmprmd <ranggadpermadi@gmail.com>
2021-12-05 11:56:26 +0700

    Setup route and models reviews for a bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,19 +9,15 @@ const { protect, authorize } = require("../middleware/auth");
 
 // include other resource routers
 const courseRouter = require("./courses");
+const reviewRouter = require("./reviews");
 
 // re-route/pass into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
+router.use("/:bootcampId/reviews", reviewRouter);
 
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 router.route("/").get(advancedResults(Bootcamp, "courses"), getBootcamps).post(protect, authorize("publisher", "admin"), createBootcamp);
 router.route("/:id").get(getBootcamp).put(protect, authorize("publisher", "admin"), updateBootcamp).delete(protect, authorize("publisher", "admin"), deleteBootcamp);
 router.route("/:id/photo").put(protect, authorize("publisher", "admin"), bootcampPhotoUpload);
-
-// router.get("/", (req, res) => {});
-// router.get("/:id", (req, res) => {});
-// router.post("/", (req, res) => {});
-// router.put("/:id", (req, res) => {});
-// router.delete("/:id", (req, res) => {});
 
 module.exports = router;
---
+++ routes/reviews.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { getReviews } = require("../controllers/reviews");
+const router = express.Router({ mergeParams: true });
+
+const Review = require("../models/Review");
+const advancedResults = require("../middleware/advancedResults");
+const { protect, authorize } = require("../middleware/auth");
+
+router.route("/").get(advancedResults(Review, { path: "bootcamp", select: "name description" }), getReviews);
+
+module.exports = router;
