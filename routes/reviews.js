0c9bfa8f259c9fb8b5c370209fb304bb7661c82b
rgdmprmd <ranggadpermadi@gmail.com>
2021-12-05 12:20:51 +0700

    Setup route to fetch single review and update seeder for review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getReviews } = require("../controllers/reviews");
+const { getReviews, getReview } = require("../controllers/reviews");
 const router = express.Router({ mergeParams: true });
 
 const Review = require("../models/Review");
@@ -7,5 +7,6 @@ const advancedResults = require("../middleware/advancedResults");
 const { protect, authorize } = require("../middleware/auth");
 
 router.route("/").get(advancedResults(Review, { path: "bootcamp", select: "name description" }), getReviews);
+router.route("/:id").get(getReview);
 
 module.exports = router;
--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ dotenv.config({ path: "./config/config.env" });
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
 const User = require("./models/User");
+const Review = require("./models/Review");
 
 // Connect to db
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true });
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex:
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8"));
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8"));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8"));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8"));
 
 // Import json into db
 const importData = async () => {
@@ -25,6 +27,7 @@ const importData = async () => {
 		await Bootcamp.create(bootcamps);
 		await Course.create(courses);
 		await User.create(users);
+		await Review.create(reviews);
 
 		console.log(`Data imported`.green.inverse);
 		process.exit();
@@ -39,6 +42,7 @@ const deleteData = async () => {
 		await Bootcamp.deleteMany();
 		await Course.deleteMany();
 		await User.deleteMany();
+		await Review.deleteMany();
 
 		console.log(`Data destroyed`.red.inverse);
 		process.exit();
