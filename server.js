5f252010edca36dfa7f3c410dbce8016335d63d7
Rangga Dimas Permadi <ranggadpermadi@gmail.com>
2021-12-14 16:11:04 +0700

    Setup limiter, http param polution prevent, and enable cors

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 const errorHandler = require("./middleware/error");
 // const logger = require("./middleware/logger");
 
@@ -49,6 +52,16 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({ windowMs: 10 * 60 * 1000, max: 100 });
+app.use(limiter);
+
+// Prevent HTTP Param Pollution
+app.use(hpp());
+
+// enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
